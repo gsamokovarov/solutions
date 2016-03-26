f616444f59bba78cf31bb5c73616906ad88e771d
Genadi Samokovarov <gsamokovarov@gmail.com>
2016-03-26 13:56:15 +0200

    Add development testing seed data

--- app/models/solution.rb
+++ app/models/solution.rb
@@ -2,10 +2,13 @@ class Solution < ApplicationRecord
   belongs_to :problem
   has_one :task, through: :problem
 
-  validates :checked_at, presence: true
   validates :content, presence: true
-  validates :test_status, presence: true
-  validates :test_output, presence: true
+
+  with_options allow_nil: true do
+    validates :checked_at, presence: true
+    validates :test_status, presence: true
+    validates :test_output, presence: true
+  end
 
   def correct?
     test_status.zero?
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.find_or_create_by!(email: 'to@example.org') do |user|
+  user.password = 'test1234'
+end
+
+task = Task.find_or_create_by!(name: "Numbers are odd") do |task|
+  task.description = 'Because the other numbers lough at them...'
+  task.test_command = 'ruby solution_test.rb'
+  task.test = <<~RUBY
+    require 'minitest/autorun'
+    require_relative 'solution'
+
+    class OddTest < Minitest::Test
+      def test_numbers_are_odd_when_they_are_like_3
+        assert_equal true, odd?(3)
+      end
+
+      def test_numbers_arent_odd_when_they_are_like_2
+        assert_equal false, odd?(2)
+      end
+    end
+  RUBY
+end
+
+problem = Problem.find_or_create_by!(user: user, task: task, ends_at: 100.years.from_now)
+
+Solution.find_or_create_by!(problem: problem) do |solution|
+  solution.content = <<~RUBY
+    def odd?(number)
+      false
+    end
+  RUBY
+end
