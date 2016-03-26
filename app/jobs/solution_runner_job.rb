11b8ae3cb3029bbf1bb8f5ad660a9c192e67930a
Genadi Samokovarov <gsamokovarov@gmail.com>
2016-03-26 13:56:40 +0200

    Introduce the initial version of SolutionRunnerJob

---
+++ app/jobs/solution_runner_job.rb
@@ -0,0 +1,15 @@
+class SolutionRunnerJob < ApplicationJob
+  def perform(solution)
+    Dir.mktmpdir do |dir|
+      File.write("#{dir}/solution_test.rb", solution.task.test)
+      File.write("#{dir}/solution.rb", solution.content)
+
+      Dir.chdir(dir) do
+        output = `#{solution.task.test_command}`
+        status = $?.exitstatus
+
+        solution.update!(test_output: output, test_status: status)
+      end
+    end
+  end
+end
