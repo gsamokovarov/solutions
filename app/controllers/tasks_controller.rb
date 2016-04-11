c3c2ccef8c1daa253d46699097005ffd964fcd24
Yuliyan Dudin <yuliyan.dudin@gmail.com>
2016-04-11 23:40:55 +0300

    Task implementation

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,24 @@
+class TasksController < ApplicationController
+  #before_action :require_login
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      flash[:status] = "OK"
+      redirect_to new_task_path, notice: "Task has been created"
+    else
+      flash[:status] = "error" 
+      redirect_to new_task_path, notice: "#{@task.errors.full_messages}"
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name,:description,:test_command,:test)
+  end
+end
